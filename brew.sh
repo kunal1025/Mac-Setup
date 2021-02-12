98fb380585c7563e1148710e4dcaf845d35a7d37
Kunal Patel <kunalp1025@gmail.com>
2021-02-12 17:45:01 -0500

    add bash file to install packages and apps

---
+++ brew.sh
@@ -0,0 +1,17 @@
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install brew packages
+brew install git
+brew install python
+brew install node
+brew install maven
+
+# Install brew cask
+brew cask install iterm2
+brew cask install intellij-idea
+brew cask install visual-studio
+brew cask install slack
+brew cask install postman
+brew cask install spotify
+brew cask install rectangle
